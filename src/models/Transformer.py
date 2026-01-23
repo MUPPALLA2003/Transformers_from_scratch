1c5f4c79b78c309590b0f202a7de8950b51ff2c5
MUPPALLA2003 <muppallakomaliitkgp2022@kgpian.iitkgp.ac.in>
2026-01-24 03:15:05 +0530

    model

---
+++ src/models/Transformer.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+from EncoderBlock import EncoderBlock
+from DecoderBlock import DecoderBlock
+from Embedding import Embeddings
+from PositionalEncoding import PositionalEncoding
+from Projection import ProjectionLayer
+
+class Transformer(nn.Module):
+    
+    def __init__(self,src_embed:Embeddings,tgt_embed:Embeddings,pos:PositionalEncoding,encoder:EncoderBlock,decoder:DecoderBlock,projection:ProjectionLayer):
+        super().__init__()
+        self.src_embed = src_embed
+        self.tgt_embed = tgt_embed
+        self.pos = pos
+        self.encoder = encoder
+        self.decoder = decoder
+        self.projection = projection
+
+    def encode(self,src,pad_mask):
+        src = self.src_embed(src)
+        src = self.pos(src)
+        return self.encoder(src,pad_mask)
+
+    def decode(self,src,encoder_output,mask,pad_mask):
+        tgt = self.src_embed(tgt)
+        tgt = self.pos(tgt)
+        return self.decode(tgt,encoder_output,mask,pad_mask) 
+    
+    def project(self,tgt):
+        return self.projection_layer(tgt)
---
+++ src/models/model.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+from Embedding import Embeddings
+from PositionalEncoding import PositionalEncoding
+from EncoderBlock import EncoderBlock
+from DecoderBlock import DecoderBlock
+from Projection import ProjectionLayer
+from Transformer import Transformer
+from LayerNormalization import LayerNormalization
+
+def build_transformer(src_vocab_size:int,tgt_vocab_size:int,seq_len:int,d_model:int,N:int,h:int,dropout:float,d_ff:int):
+    src_embed = Embeddings(d_model,src_vocab_size)
+    tgt_embed = Embeddings(d_model,tgt_vocab_size)
+    pos = PositionalEncoding(d_model,seq_len,dropout)
+    norm = LayerNormalization()
+    encoder = EncoderBlock(src_embed,pos,norm,dropout,N,d_model,d_ff,h)
+    decoder = DecoderBlock(tgt_embed,pos,dropout,N,d_model,d_ff,h)
+    projection = ProjectionLayer(d_model,tgt_vocab_size)
+    transformer = Transformer(src_embed,tgt_embed,pos,encoder,decoder,projection)
+
+    for p in transformer.parameters():
+        if p.dim()>1:
+            nn.init.xavier_uniform_(p)
+
+    return transformer        
